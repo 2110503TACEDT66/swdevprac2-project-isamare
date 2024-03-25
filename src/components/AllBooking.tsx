4ed19c788886fe636be9b5de2321e286db914210
sherlyzi <crasrachananut@gmail.com>
2024-03-25 12:40:27 +0700

    book button

--- src/components/AllBooking.tsx
+++ src/components/AllBooking.tsx
@@ -13,10 +13,10 @@ export default async function AllBooking({bookingsJson}:{bookingsJson:Promise<Bo
                bookingsJsonReady.data.map((BookingItem2:BookingItem2)=>(
                 <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2" key={BookingItem2.createAt}>
                 <div className="text-xl">Reserve at {BookingItem2.coworking.name}</div>
-                <div className="text-sm">From {BookingItem2.start} to {BookingItem2.end}</div>
+                <div className="text-md">From {BookingItem2.start} to {BookingItem2.end}</div>
                 <div className="text-md">Date {BookingItem2.apptDate.split('T')[0]}</div>
                 <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
-                >Remove</button>
+                >Remove this</button>
             </div>
                ))
             }
