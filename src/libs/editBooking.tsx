1a20900fcc820861773e8e110da98a79e404537b
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-03-27 02:17:14 +0700

    .

--- src/libs/editBooking.tsx
+++ src/libs/editBooking.tsx
@@ -3,6 +3,7 @@ import { BookingItemEdit } from "../../interface"
 
 export default async function editBooking(token: string,bookingId: string, apptDate:string,start:string,end:string){
    const response = await fetch(`${process.env.BACKEND_URL}/api/project/reservations/${bookingId}`,{
+      cache: "no-store",
        method: "PUT",
        headers: {
          "Content-type":"application/json",
