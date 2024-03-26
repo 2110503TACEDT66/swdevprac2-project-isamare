ec4b0d8f1b9098634da22c31fcfb7d34d6447570
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-03-27 02:13:25 +0700

    .

--- src/app/(user)/profile/edit/[uid]/page.tsx
+++ src/app/(user)/profile/edit/[uid]/page.tsx
@@ -131,14 +131,14 @@ export default function EditProfileUser({params}:{params:{uid:string}}) {
               </div>
 
               <div>
-                <Link href={'/'}>
+                
                   <button
                   className="flex w-[40%] m-auto justify-center  rounded-md bg-[#252645] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-gradient-to-r hover:from-[#252645] hover:to-[#5C5EAB]"
                   type="submit"
                   >
                     {hasEdit ? "complete" : "Edit"}
                   </button>
-                </Link>
+                
                 
               </div>
             </form>
